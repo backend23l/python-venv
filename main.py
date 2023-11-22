f3bd6da2d1b4d66352548ae1ad76ae88833214d9
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-22 17:49:20 +0500

    write users

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import requests
 import json
-from pprint import pprint
 
 
-response = requests.get('https://randomuser.me/api/')
-
-if response.status_code == 200:
-    content = response.content
-
-    data = json.loads(content.decode())
+def get_user():
+    response = requests.get('https://randomuser.me/api/')
     
-    randomuser = data['results'][0]
-
-    user = {
-        "fullname": f"{randomuser['name']['first']} {randomuser['name']['last']}",
-        "email": randomuser['email'],
-        "phone": randomuser['phone'],
-        "age": randomuser['dob']['age'],
-    }
-    print(user)
+    if response.status_code == 200:
+        content = response.content
     
+        data = json.loads(content.decode())
+    
+        randomuser = data['results'][0]
+    
+        user = {
+            "fullname": f"{randomuser['name']['first']} {randomuser['name']['last']}",
+            "email": randomuser['email'],
+            "phone": randomuser['phone'],
+            "age": randomuser['dob']['age'],
+        }
+    return user
+
+def get_n_users(n: int) -> list[dict]:
+    users = []
+    for i in range(n):
+        user = get_user()
+        users.append(user)
+
+    return {"users": users, "info": {"count": n}}
+
+def write_users(users: dict):
+    with open('users.json', 'w') as f:
+        data_json = json.dumps(users, indent=4)
+        f.write(data_json)
+
+users = get_n_users(31) 
+write_users(users)
