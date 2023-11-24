5347d02c2ca3e7046da1ea88d6c5b2bb4d23f5d2
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-11-24 15:46:23 +0500

    main

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+import json 
 import requests
-import json
 
+def get_user(gen):
+    response = requests.get('https://randomuser.me/api/')
+    if response.status_code==200:
+        content = response.content
+        data = json.loads(content.decode())
+        randomuser = data["results"][0]
+        user = None
+        if randomuser['gender']==gen:
+            user = {
+                "gender":randomuser["gender"],
+                "first":randomuser['name']['first'],
+                "last":randomuser['name']['last'],
+                "email":randomuser['email'],
+                "phone":randomuser['phone'],
+                "age":randomuser['dob']['age'],
+            }
+    return user
 
-response = requests.get('https://randomuser.me/api/')
+def get_n_users(n:int,gen:str):
+    users = []
+    i = 0
+    while i < n:
+        user = get_user(gen)
+        if user != None:
+            users.append(user)
+            i -= 1
+        i += 1
 
-if response.status_code == 200:
-    content = response.content
+    return {"users":users,"info":{"Count":n}}
 
-    data = json.loads(content.decode())
-    print(data)
+def write_users(use:dict):
 
-{
-    "fullname": "fadsfsad",
-    "email": "dfasdfas",
-    "phone": "fasdfdsa",
-    "age": 231
-}
+    with open("male_female.json",'w') as f:
+        data_json = json.dumps(use,indent=4)
+        f.write(data_json)
+
+users = get_n_users(9,"male")
+write_users(users)
